5644cc51ea1236a18c8d2e21c2b6e05972d2845d
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-04-05 01:31:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","19525325"))
 API_HASH = getenv("API_HASH","ddc123dd015945669f20498bd0a50409")
 
-BOT_TOKEN = getenv("BOT_TOKEN","6733899123:AAGSF2E0-_kMHbCwOCc5gJ7BH06vdIkKehg")
+BOT_TOKEN = getenv("BOT_TOKEN","6733899123:AAHIOd2Bg_BH8LQGrztzwRkO3D60t_-I_qE")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mohamedhelal:mohamedhelal@cluster0.qhhuj2p.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 6909581339))
