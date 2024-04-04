8c312a21e85255d85b14e5b5cd572d2b88fe16e6
VeGaZoRo <166079069+VeGaZoRo@users.noreply.github.com>
2024-04-05 01:22:05 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID","19525325"))
+API_HASH = getenv("API_HASH","ddc123dd015945669f20498bd0a50409")
+
+BOT_TOKEN = getenv("BOT_TOKEN","6733899123:AAGSF2E0-_kMHbCwOCc5gJ7BH06vdIkKehg")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mohamedhelal:mohamedhelal@cluster0.qhhuj2p.mongodb.net/?retryWrites=true&w=majority")
+
+OWNER_ID = int(getenv("OWNER_ID", 6909581339))
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VeGaSource")
